2d6354f95036917dfa2c482c5bcb1588f3c5c3fd
geralfaro <german.alfaro@unc.edu.ar>
2019-10-01 22:17:37 -0300

    Archivo app/test.py editado

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TransactionTestCase
+from django.test.runner import DiscoverRunner
+from unittest.suite import TestSuite
 
-# Create your tests here.
+class UnitTestRunner(DiscoverRunner):
+    def setup_databases(self, **kwargs):
+        pass
+    def teardown_databases(self, old_config, **kwargs):
+        pass
+    def build_suite(self, **kwargs):
+        suite = super().build_suite(**kwargs)
+        tests = [t for t in suite._tests if self.is_unittest(t)]
+        return TestSuite(tests=tests)
+    def is_unittest(self, test):
+        return not issubclass(test.__class__, TransactionTestCase)
