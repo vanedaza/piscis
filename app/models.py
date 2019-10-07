2c150a819bbf29807b121fbdb02ee9428bcab9d5
geralfaro <german.alfaro@unc.edu.ar>
2019-10-07 18:54:40 -0300

    Agregada la descripción de los modelos en app/model.py

--- app/models.py
+++ app/models.py
@@ -1,13 +1,15 @@
 from django.db import models
    
 class Imagen(models.Model):
+    """"Upload the image to be voted. The uploaded images are saved in a gallery"""
     image = models.ImageField(upload_to = 'app', default = 'app/static/galaxias/img1.jpeg')
     name = models.CharField(max_length=200)
 
 class Choice(models.Model):
+    """"For a uploaded image, allow select between three different types one to classify it"""
     imagen = models.ForeignKey(Imagen, on_delete=models.CASCADE,  blank=True, null=True)
     CHOICE_TEXT = (('A', 'Alta'), ('B', 'Baja'), ('C','Ninguna'))
-    Que_tipo_de_interacción_ves = models.CharField(max_length=200, choices=CHOICE_TEXT, default='A')
+    Que_tipo_de_interacción_ves = models.CharField(max_length=200, choices=CHOICE_TEXT)
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.Que_tipo_de_interacción_ves
