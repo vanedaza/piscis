fa8c3c0b8b00a3a8f5dfb040fbcd854abb82ae2d
josegit88 <jose.astroph@gmail.com>
2019-10-21 19:03:13 -0300

    Se realizó un script siguiendo el ejemplo con la licencia y agregado al inicio de todos los archivos .py del proyecto \n También se agregaron imágenes de galaxias de Galaxy-Zoo de Keel W. et al. 2013 en la base de datos

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,9 @@
+# This file is part of the:
+#   Piscis Project (https://github.com/mkoraj/piscis).
+# Copyright (c) 2019-2020, PISCIS
+# License: BSD-3-Clause
+#   Full Text: https://github.com/mkoraj/piscis/blob/master/LICENSE
+
 from django.urls import path
 
 from . import views
