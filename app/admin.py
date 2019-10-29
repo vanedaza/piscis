c25ef806b9873158248f9daaa73052a9fa577b8d
josegit88 <jose.astroph@gmail.com>
2019-10-29 11:17:58 -0300

    Se modificó la Licencia de los archivos .py

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,8 @@
 # This file is part of the:
-#   Piscis Project (https://github.com/mkoraj/piscis).
-# Copyright (c) 2019-2020, PISCIS
-# License: BSD-3-Clause
-#   Full Text: https://github.com/mkoraj/piscis/blob/master/LICENSE
+# 	Piscis Project (https://github.com/mkoraj/piscis).
+# Copyright (c) 2019 Koraj, Mauricio; Lares, Marcelo; Alfaro, Germán; Santucho,Victoria; Benavides,Jose & Daza, Ingrid All rights reserved.
+# 	License: MIT License
+# Full Text: https://github.com/mkoraj/piscis/blob/master/LICENSE
 
 from django.contrib import admin
 from .models import Choice
--- app/tests/test_view.py
+++ app/tests/test_view.py
@@ -1,7 +1,7 @@
 # This file is part of the:
 #   Piscis Project (https://github.com/mkoraj/piscis).
-# Copyright (c) 2019-2020, PISCIS
-# License: BSD-3-Clause
+# Copyright (c) 2019 Koraj, Mauricio; Lares, Marcelo; Alfaro, Germán; Santucho,Victoria; Benavides,Jose & Daza, Ingrid All rights reserved.
+# License: MIT License
 #   Full Text: https://github.com/mkoraj/piscis/blob/master/LICENSE
 
 from django.test import TestCase
