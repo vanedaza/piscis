c45123b2c81e9ffc9d07b0f507f52dd49dd13eca
mkoraj <mauricio.koraj@gmail.com>
2019-10-30 18:21:59 -0300

    minor fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,13 +30,13 @@ ALLOWED_HOSTS= ['127.0.0.1', '.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin', 
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'encuesta.apps.EncuestaConfig'
+    'encuesta'
 ]
 
 MIDDLEWARE = [
