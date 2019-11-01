f49605b39b6d5728aac86fbe0ce75141b34f9592
geralfaro <german.alfaro@unc.edu.ar>
2019-11-01 17:50:04 -0300

    Guarda datos de encuestas, editados views y urls

--- encuesta/urls.py
+++ encuesta/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from . import views
 
-
-
 urlpatterns = [
         path('', views.pag_ppal, name = 'pag_ppal'),
 	path('pag_ppal/contact_det/', views.contact_det, name= 'contact_det'),
 	path('pag_ppal/desc_det/', views.desc_det, name= 'desc_det'),
 	path('pag_ppal/app_encuesta/', views.app_encuesta, name= 'app_encuesta'),
-	path('pag_ppal/app_encuesta/user/',views.user, name='user'),
+#	path('pag_ppal/new', views.choice_new, name='choice_new'),
 ]
--- encuesta/views.py
+++ encuesta/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import *
 from .forms import ChoiceForm
+from django.shortcuts import redirect
 
 
 def pag_ppal(request):
@@ -15,9 +16,21 @@ def desc_det(request):
 def user(request):
     return render(request, 'encuesta/user.html',{})
 
-def app_encuesta(request):
-	form = ChoiceForm()
+#def app_encuesta(request):
+#	form = ChoiceForm()
 #	form.save()
-	return render(request, 'encuesta/app_encuesta.html',{'form':form})
+#	return render(request, 'encuesta/app_encuesta.html',{})
 
+#def choice_new(request):
+def app_encuesta(request):
+	if request.method == "POST":
+		form = ChoiceForm(request.POST)
+		if form.is_valid():
+			choice = form.save(commit=False)
+			choice.usuario = request.user
+			choice.save()
+			return redirect('app_encuesta')
+	else:
+		form = ChoiceForm()
+		return render(request, 'encuesta/app_encuesta.html',{'form':form})
 
