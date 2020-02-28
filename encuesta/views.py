7de955ad83ef3f3c915b4a60fa6f0283004df6df
vanedaza <vizes64@hotmail.com>
2020-02-28 00:24:47 -0300

    n

--- encuesta/views.py
+++ encuesta/views.py
@@ -9,14 +9,20 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.shortcuts import redirect
 from .models import *
-from .forms import ChoiceForm
-from .forms import AstronomerForm
+from .forms import ChoiceForm, AstronomerForm
 from django.contrib.auth import logout as do_logout
 from django.contrib.auth import login as do_login
 from django.contrib.auth import authenticate
 from django.contrib.auth.forms import AuthenticationForm
+import random
+from django.template import RequestContext, Context
+from django.shortcuts import render_to_response
+from django.template.response import TemplateResponse
 
-
+#from django.views.generic import ListView
+#class HomePageView(ListView):
+#    model = Images
+#    template_name = 'encuesta/app_encuesta.html'
 
 def pag_ppal(request):
     return render(request, 'encuesta/pag_ppal.html',{})
@@ -27,21 +33,28 @@ def contact_det(request):
 def desc_det(request):
     return render(request, 'encuesta/desc_det.html',{})
 
-def app_encuesta(request):
-#	all_choices = models.Choice.objects.all()
-#	image = random.choice(all_choices)
-	if request.method == "POST":
-		form = ChoiceForm(request.POST)
-		if form.is_valid():
-			choice = form.save(commit=False)
-			choice.usuario = request.user
-#			choice.cover = request.cover
-			choice.save()
-			return redirect('app_encuesta')
-	else:
-		form = ChoiceForm()
-		return render(request, 'encuesta/app_encuesta.html',{'form':form})
 
+
+def app_encuesta(request):
+    pks = Images.objects.values_list('pk', flat=True) # flat=True will remove the tuples and return the list 
+    random_idx = random.randint(0, len(pks)-1) #tira un numero random
+    prueba = list(Images.objects.all()) #crea una lista con las imágenes
+    if request.method == "GET": # GET se ejecuta al ingresar o recargar la página
+        Choice.objects.filter(voto__exact='').delete() # remueve todas las encuestas que no tengan voto
+        form=ChoiceForm() # Crea el formulario
+        choice = form.save(commit=False) 
+        choice.imagen = Images.objects.get(pk=pks[random_idx]) # guarda la imagen random
+        choice.usuario = request.user # guarda el usuario
+        choice.save() # guarda en el formulario el pk, el usuario y la imagen
+        return render(request, 'encuesta/app_encuesta.html', {'form': form, 'prueba_images': prueba[random_idx]})
+    if request.method == "POST": # POST se ejecuta después de votar        
+        form = ChoiceForm(request.POST)        #construir el PostForm con datos del voto
+        if form.is_valid():                    #verifica si todos los datos son correctos
+            choice = form.save(commit=False)   #commit=False significa que no queremos guardar el modelo Choice, primero queremos guardar lo que sigue
+            choice_id=Choice.objects.values_list('pk',flat=True) # crea una lista con todos los pk de las encuesta
+            Choice.objects.filter(pk=max(choice_id)).update(voto=choice.voto) # Va a la última encuesta y agrega el vovot seleccionado
+            return redirect('app_encuesta')
+     
 def welcome(request):
     if request.user.is_authenticated:
         return render(request, 'encuesta/welcome.html')
--- mysite/settings.py
+++ mysite/settings.py
@@ -122,7 +122,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'                        # es la URL que podemos usar en nuestras plantillas para los archivos
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  #ruta absoluta de los archivos cargados por el usuario
 
 LOGIN_REDIRECT_URL = "index"
