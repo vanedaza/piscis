d200794c18ff28de362377925296a336f992078a
vanedaza <vizes64@hotmail.com>
2020-08-11 13:34:48 -0300

    espacio

--- encuesta/forms.py
+++ encuesta/forms.py
@@ -20,4 +20,4 @@ class AstronomerForm(UserCreationForm):
 
     class Meta:
         model = User
-            fields = ["username", "email", "password1", "password2", ]
+        fields = ["username", "email", "password1", "password2", ]
