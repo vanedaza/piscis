4a0807987b4dc9c0e65a632afa7128d0a381ff5c
vanedaza <vizes64@hotmail.com>
2020-11-20 18:24:56 -0300

    ruta de imágenes static

--- piscis/settings.py
+++ piscis/settings.py
@@ -117,9 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
 
 MEDIA_URL = '/media/'                        # es la URL que podemos usar en nuestras plantillas para los archivos
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  #ruta absoluta de los archivos cargados por el usuario
