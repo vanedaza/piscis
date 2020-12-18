95ecc0f10939610eb5ad2197d0f14b8c6a877d41
geralfaro <german.alfaro@unc.edu.ar>
2020-12-18 19:09:18 -0300

    subido el test_views de la pagina inicio

---
+++ encuesta/tests/test_views.py
@@ -0,0 +1,27 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class TestPage(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_pag_inicio(self):
+        # Verifica si el código responde
+        url = reverse("inicio")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_pag_inicio2(self):
+        # Verifica el template usado
+        url = reverse("inicio")
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "encuesta/inicio.html")
+
+    def test_pag_inicio3(self):
+        # Verifica el template muestre las palabras "Encuesta", "Iniciar seción", "registrate"
+        url = reverse("inicio")
+        response = self.client.get(url)
+        self.assertContains(response, "Encuesta")
+        self.assertContains(response, "iniciar_sesion")
+        self.assertContains(response, "registrate")
