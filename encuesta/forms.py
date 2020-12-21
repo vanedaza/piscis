7bfc96823b936bd7982a04a92c63af01be030b78
vanedaza <vizes64@hotmail.com>
2020-12-21 20:39:16 -0300

    docstyle

--- encuesta/forms.py
+++ encuesta/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
 from .models import Choice
 
 
 class ChoiceForm(forms.ModelForm):
+    """Ingresa la información del voto por el usuario
+     y el id de la imagen elegida"""
+
     class Meta:
         model = Choice
         fields = (
@@ -15,6 +17,8 @@ class ChoiceForm(forms.ModelForm):
 
 
 class AstronomerForm(UserCreationForm):
+    """Información del usuario"""    
+
     email = forms.EmailField(label="Correo electrónico")
 
     class Meta:
