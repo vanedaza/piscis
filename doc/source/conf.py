c1dd942d6cf517973a1137b4fafd22d956a0d004
vanedaza <vizes64@hotmail.com>
2020-12-22 20:37:37 -0300

    sin paquete

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -41,9 +41,7 @@ extensions = [
 
     'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
-    'sphinx.ext.autosummary',
-
-    'nbsphinx'
+    'sphinx.ext.autosummary'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
